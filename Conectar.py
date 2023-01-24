2908404444a11fd44f960b20e2ab45dcf51141a9
TioSupply <112595367+TioSupply@users.noreply.github.com>
2023-01-24 20:14:42 -0300

    Update Conectar.py

--- Conectar.py
+++ Conectar.py
@@ -1,26 +1,15 @@
 import mysql.connector
 
-class Conectar:
-    def __init__(self):
-        self.conexion = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="formativa"
-        )
-        self.cursor = self.conexion.cursor()
-    
-    def ejecutar(self, sql):
-        self.cursor.execute(sql)
-        self.conexion.commit()
-        return "La conexion fue exitosa ..."
-  
-    def listarTodos(self, sql):
-        self.cursor.execute(sql)
-        resultados = self.cursor.fetchall()
-        return resultados
-    
-    def listarUno(self, sql):
-        self.cursor.execute(sql)
-        resultado = self.cursor.fetchone()
-        return resultado
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user ="root",
+    password = "",
+    database = "formativa"
+)
+
+mycursor = mydb.cursor ()
+
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+    print(x)
