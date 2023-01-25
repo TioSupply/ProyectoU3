a3766668c1ccc6c4c8a52760b5193c6926f63b41
TioSupply <112595367+TioSupply@users.noreply.github.com>
2023-01-25 00:06:56 -0300

    Update Ciudad.py

--- Ciudad.py
+++ Ciudad.py
@@ -1,10 +1,31 @@
-from Conectar import Conectar
+import mysql.connector
 
-class Ciudad:
+class Conectar:
     def __init__(self):
-        self.conectar = Conectar()
+        self.__db = mysql.connector.connect(
+            host = 'localhost',
+            user = 'root',
+            password = '', 
+            database = 'formativa'
+        )
+        
+        self.__cursor = self.__db.cursor()
 
-    def listarCiudades(self):
-        sql = "SELECT * FROM ciudades"
-        resultados = self.conectar.listarTodos(sql)
-        return resultados
+    def ejecutar(self, sql):
+        try:
+            self.__cursor.execute(sql)
+            self.__db.commit()
+            resultado = self.__cursor.rowcount
+            return resultado
+        except mysql.connector.Error as e:
+            return 'Se ha producido un error: '+str(e)
+
+    def listarTodos(self, sql):
+        self.__cursor.execute(sql)
+        lista = self.__cursor.fetchall()
+        return lista
+
+    def listarUno(self, sql):
+        self.__cursor.execute(sql)
+        tupla = self.__cursor.fetchone()
+        return tupla
